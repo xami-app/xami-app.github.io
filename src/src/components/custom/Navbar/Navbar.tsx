242a65847d38b178fdbac5845fc5c3c35a3ebcf3
Timon (bei Chiara) <timon.mondry@gmail.com>
2025-04-19 10:44:16 +0200

    Cleanup

--- src/src/components/custom/Navbar/Navbar.tsx
+++ src/src/components/custom/Navbar/Navbar.tsx
@@ -8,7 +8,7 @@ export const Navbar: React.FC<NavbarProps> = ({ className = "" }) => {
     return (
         <div className={`fixed top-0 left-0 w-full flex justify-between items-center px-6 py-3 bg-transparent backdrop-blur-md shadow-md z-10 ${className}`}>
             <div className="flex-grow flex">
-                <input placeholder="Here comes a search!"></input>
+                <input placeholder="Searchbar implementation is coming in a future update!"></input>
             </div>
         </div>
     );
--- src/vite.config.ts
+++ src/vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
-    outDir: '../docs',
+    outDir: '../out',
     emptyOutDir: true,
   },
 })
