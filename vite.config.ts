f9b73ce09b9b0d7561c6f93fd68e4977ccbbad74
Timon (bei Chiara) <timon.mondry@gmail.com>
2025-04-19 15:20:52 +0200

    correct out dir

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
-    outDir: 'out',
+    outDir: 'docs',
     emptyOutDir: true,
   },
 })
